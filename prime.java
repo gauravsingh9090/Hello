783061348cc6e2ab0f7e172df3dfbe7be4e71958
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-06-28 13:28:03 +0530

    prime code to be generate

---
+++ prime.java
@@ -0,0 +1,21 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " is a prime number");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
