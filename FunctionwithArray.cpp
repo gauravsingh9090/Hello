1c1909f50318b1b256a226a9e11498cf741a13bf
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-07-05 10:21:07 +0530

    Create FunctionwithArray.cpp

---
+++ FunctionwithArray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void func(int arr[5]){
+     for(int i=0;i<5;i++){
+       cout<<arr[i]<<"\n";
+}
+}
+int main(){
+      int ar[5] = {2,4,6,7,8};
+     func(ar);
+return 0;
+}
