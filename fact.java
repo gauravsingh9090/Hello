922b0aeaf4d84fba59516643779c53ed5fe517a6
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-07-10 00:07:17 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -14,3 +14,24 @@ public class Factorial {
         return n * factorial(n - 1);
     }
 }
+
+////////////////////////////////////////////
+class Main{
+     public static void main(String args[]){
+         int n=5 ;
+         System.out.println(fact(n));
+         
+     }
+     public static int fact(int n){
+         int res =1;
+         for(int i=2;i<=n;i++){
+             if(n==1|| n==0){
+                 return 1;
+             }
+             else {
+                 res = res*i;
+             }
+         }
+         return res;
+     }
+}
