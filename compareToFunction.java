449c51bd916c706fe53f10766a1d8471300d92e4
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-07-23 15:09:38 +0530

    Create compareToFunction.java

---
+++ compareToFunction.java
@@ -0,0 +1,18 @@
+// compareTo function 
+
+import java.util.Scanner ;
+
+class HelloWorld {
+    public static void main(String[] args) {
+     String fname = "Zero";
+     String sname = "Aman";
+    if(fname.compareTo(sname)==0){
+        System.out.println("String are equal ");
+        
+    }
+    else{
+        System.out.println("String is not equAL");
+    }
+     // 
+    }
+}
