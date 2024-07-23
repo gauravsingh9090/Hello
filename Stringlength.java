d86449ccd37cc278a19ed72eff292c325452989a
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-07-23 14:58:02 +0530

    Create Stringlength.java

---
+++ Stringlength.java
@@ -0,0 +1,14 @@
+// String length  are
+import java.util.Scanner ;
+
+class HelloWorld {
+    public static void main(String[] args) {
+     String fname = "Aman";
+     String sname = "Patel";
+     String fullname = fname +" "+ sname;
+     System.out.println(fullname);
+     System.out.println(fullname.length());
+     
+     // 
+    }
+}
