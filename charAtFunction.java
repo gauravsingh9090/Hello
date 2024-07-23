6ae12f9b8792dd4b91da30f19defd3e7c2f5e682
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-07-23 15:00:18 +0530

    Create charAtFunction.java

---
+++ charAtFunction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner ;
+
+class HelloWorld {
+    public static void main(String[] args) {
+     String fname = "Aman";
+     String sname = "Patel";
+     String fullname = fname +" "+ sname;
+     System.out.println(fullname);
+     System.out.println(fullname.length());
+     for(int i=0;i<=fullname.length();i++){
+         System.out.println(fullname.charAt(i));
+     }
+     // 
+    }
+}
