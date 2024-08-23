b511dea4124e37a9ff611f15ece704bcd885be29
Aman Patel <128533775+patelaaman@users.noreply.github.com>
2024-08-23 11:27:21 +0530

    Create A1.jsx

---
+++ A1.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
